5c6b975f743b7754b40dc741c49b24731cdba198
aniket chakke <aniketchakke63@gmail.com>
2026-01-05 13:05:33 +0530

    added svgs

--- src/Pages/MyProjects.jsx
+++ src/Pages/MyProjects.jsx
@@ -173,21 +173,12 @@ export default function MyProjects() {
                   />
 
                   {/* Delete */}
-                  <svg
+                  <img
+                    src={trash}
+                    alt="Delete"
                     onClick={() => handleDelete(p.id)}
-                    xmlns="C:\Users\hP\Documents\Aniket\PROJECTS\Agency\Dashboard Demo\frontend\src\assets\trash.svg"
-                    className="h-5 w-5 text-red-600 cursor-pointer hover:scale-110"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
+                    className="h-5 w-5 cursor-pointer hover:scale-110"
+                  />
                 </td>
               </tr>
             ))}
